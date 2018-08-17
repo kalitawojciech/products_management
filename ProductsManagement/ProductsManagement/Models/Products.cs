c4d3d5d00af2de51d84a40ca11e97fbabd28370c
Wojciech Kalita <wojtek1311997@gmail.com>
2018-08-17 11:55:33 +0200

    Models creation - part I
    Signed-off-by: Wojciech Kalita <wojtek1311997@gmail.com>

---
+++ ProductsManagement/ProductsManagement/Models/Products.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductsManagement.Models
+{
+    public class Products
+    {
+        public int Products_ID { get; set; }
+        public string Product_Name { get; set; }
+        public string Product_Description { get; set; }
+        public int category_ID { get; set; }
+        public double Products_Price { get; set; }
+    }
+}

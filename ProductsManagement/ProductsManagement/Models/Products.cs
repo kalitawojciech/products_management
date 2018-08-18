ca03fb1a0ae56cd134de4d82dfd67a8410a5e924
Wojciech Kalita <wojtek1311997@gmail.com>
2018-08-18 18:29:16 +0200

    Add validation

--- ProductsManagement/ProductsManagement/Models/Products.cs
+++ ProductsManagement/ProductsManagement/Models/Products.cs
@@ -10,15 +10,24 @@ namespace ProductsManagement.Models
     {
         
         public int ID { get; set; }
-        
+        [Display(Name ="Product's Name")]
         [StringLength(60, MinimumLength = 3)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""-]*$", ErrorMessage = "Product's name must consist letters, numbers, $*-+")]
         [Required]
         public string Product_Name { get; set; }
 
+        [Display(Name = "Product's Description")]
         [Required]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""-]*$", ErrorMessage = "Product's dascription must consist letters, numbers, $*-+")]
         [StringLength(120, MinimumLength = 3)]
         public string Product_Description { get; set; }
+
+        [Display(Name = "Product's Category")]
+        [Required]
         public int category_ID { get; set; }
+
+        [Display(Name = "Product's Price")]
+        [DataType(DataType.Currency)]
         public double Products_Price { get; set; }
 
         public virtual Category Category { get; set; }
